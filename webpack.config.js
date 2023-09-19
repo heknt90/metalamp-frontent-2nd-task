43534a6fdaaf0cbd5b328fb3be515d98e1e07428
Nikita Efremov <heknt90@mail.ru>
2023-09-19 18:10:10 +0300

    Logo component added Alias Components added to webpack config Mixin logo added to ./components/logo/logo.pug uikit.layout.pug added to ./layouts logo images added to ./components/logo/images/ Page uikit added Link to uikit.html added at index page

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ const getPagePath = (page) => "./src/pages/" + page
 const config = {
     target: "web",
     entry: {
-        index: getPagePath('index/index.pug')
+        index: getPagePath('index/index.pug'),
+        uikit: getPagePath("uikit/uikit.pug"),
     },
     output: {
         path: path.resolve(__dirname, 'dist/'),
@@ -23,6 +24,7 @@ const config = {
             Layouts: path.join(__dirname, "./src/layouts/"),
             SCSS: path.join(__dirname, "./src/scss/"),
             Containers: path.join(__dirname, "./src/containers/"),
+            Components: path.join(__dirname, "./src/components/"),
         }
     },
     module: {
