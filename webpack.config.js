edc25bdf1fb693e7d3b2380436cc818dd567c778
Nikita Efremov <heknt90@mail.ru>
2023-09-19 16:09:28 +0300

    webpack config updated clean-webpack-plugin deleted deleted unnecessary files created and enabled new entry point (pages/index/index.pug)

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,36 @@
 const path = require('path')
 const PugPlugin = require('pug-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin  = require('copy-webpack-plugin')
 
-const sourcePath = path.join(__dirname, 'src');
-const keepPugFolderStructure = (pathData) => {
-    const sourceFile = pathData.filename;                       
-    const relativeFile = path.relative(sourcePath, sourceFile); 
-    const { dir, name } = path.parse(relativeFile);             
-    return `${dir}/${name}.html`;                         
-};
+const isProduction = process.env.NODE_ENV == "production"
+const getPagePath = (page) => "./src/pages/" + page
 
-module.exports = {
-    mode: "production",
+const config = {
+    target: "web",
     entry: {
-        index:'./src/index.pug',
-        "ui-colors-and-types": {
-            import: "./src/ui-kit/color-and-types.pug",
-            filename: keepPugFolderStructure
-        }
+        index: getPagePath('index/index.pug')
     },
     output: {
-        path: path.resolve(__dirname, 'dist/')
+        path: path.resolve(__dirname, 'dist/'),
+        clean: true,
     },
     devServer: {
-        port: 4200,
-        liveReload: true,
+        open: true,
         watchFiles: ["src/**/*"]
     },
-    target: "web",
-    plugins: [
-        new PugPlugin({
-            pretty: true,
-            js: {
-                filename: 'js/[name].[contenthash:8].js'
-            },
-            css: {
-                filename: 'css/style.[contenthash:8].css'
-            }
-        }),
-        require('autoprefixer'),
-        new CopyWebpackPlugin({
-            patterns: [
-                {from: './src/assets/favicons', to: ""}
-            ]
-        }
-        ),
-        new CleanWebpackPlugin()
-    ],
     module: {
         rules: [
             {
                 test: /\.pug$/,
                 loader: PugPlugin.loader,
-                exclude: ['/node_modules']
             },
             {
                 test: /\.scss$/,
-                use: ['css-loader', 'postcss-loader', 'sass-loader'],
+                use: [
+                    'css-loader', 
+                    'postcss-loader', 
+                    'sass-loader'
+                ],
             },
             {
                 test: /\.(jpg|jpeg|png|webp|svg)$/,
@@ -75,5 +48,32 @@ module.exports = {
                 }
             },
         ]
+    },
+    plugins: [
+        new PugPlugin({
+            pretty: true,
+            js: {
+                filename: 'js/[name].[contenthash:8].js'
+            },
+            css: {
+                filename: 'css/style.[contenthash:8].css'
+            }
+        }),
+        require('autoprefixer'),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: './src/assets/favicons', to: "images/favicons"}
+            ]
+        }
+        ),
+    ],
+}
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = "production"
+    } else {
+        config.mode = "development"
     }
+    return config
 }
